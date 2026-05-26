ed39be401377534c5fac3aa07d0e5c163a4ea1b8
Filip Bergander <filip.bergander1996@gmail.com>
2026-05-26 15:24:14 +0200

    Lagt till validering om man är admin eller personal, går inte skapa en ny användare med personal-behörighet.

--- src/js/authentication/checkAuth.js
+++ src/js/authentication/checkAuth.js
@@ -2,12 +2,23 @@
 checkAuthAccess();
 // Ser över om användaren har behörighet till admin-sidan
 export function checkAuthAccess() {
-    const adminUser = document.getElementById("admin-user");
+
+    // Routes som kräver inloggning
+    const protectedRoutes = [
+        "/index.html",
+        "/register.html",
+        "/"
+    ]
+
+    // Sökväg
+    const userPath = window.location.pathname;
 
     // Går inte att navigera till admin-sidan om användaren inte är inloggad
-    if (window.location.pathname.endsWith("index.html") && !localStorage.getItem("login-key") && !localStorage.getItem("role") && !localStorage.getItem("username")) {
-        adminUser.innerHTML = "";
+    if (protectedRoutes.includes(userPath) && !localStorage.getItem("login-key")) {
         window.location.href = "login.html"; // Redirect till login-sidan
-    };
-
+    }
+    // Förhindrar att vanliga användare som personal kan navigera till registera-sidan
+    if (userPath === "/register.html" && localStorage.getItem("role") !== "admin") {
+        return window.location.href = "index.html"; // Redirect till index-sidan
+    }
 }
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: resolve(__dirname, "index.html"),
+                register: resolve(__dirname, "register.html"),
                 login: resolve(__dirname, "login.html")
             }
         }
